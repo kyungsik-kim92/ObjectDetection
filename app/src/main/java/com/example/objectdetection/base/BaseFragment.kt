cba6d989edc1ded71cbd28b930b67a83f9eedbee
Kim Kyungsik <ksiki1@naver.com>
2024-03-09 19:59:25 +0900

    Base 추가

--- app/src/main/java/com/example/objectdetection/base/BaseFragment.kt
+++ app/src/main/java/com/example/objectdetection/base/BaseFragment.kt
@@ -8,24 +8,35 @@ import androidx.annotation.LayoutRes
 import androidx.databinding.DataBindingUtil
 import androidx.databinding.ViewDataBinding
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.lifecycleScope
+import com.example.objectdetection.BR
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.map
 
 abstract class BaseFragment<B : ViewDataBinding>(@LayoutRes private val layoutId: Int) :
     Fragment() {
 
     protected lateinit var binding: B
 
-    protected val baseActivity: BaseActivity<*>?
-        get() = activity as BaseActivity<*>?
+    abstract val viewModel: BaseViewModel
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        setHasOptionsMenu(true)
         binding = DataBindingUtil.inflate(inflater, layoutId, container, false)
         binding.lifecycleOwner = this
+        binding.setVariable(BR.viewModel, viewModel)
+        initUi()
+        viewModel.viewState.map(::onChangedViewState).launchIn(viewLifecycleOwner.lifecycleScope)
+        viewModel.viewEvent.map(::onChangeViewEvent).launchIn(viewLifecycleOwner.lifecycleScope)
         return binding.root
     }
+    abstract fun initUi()
+
+    abstract fun onChangedViewState(state: ViewState)
+
+    abstract fun onChangeViewEvent(event: ViewEvent)
 
 }
--- app/src/main/java/com/example/objectdetection/base/BaseViewModel.kt
+++ app/src/main/java/com/example/objectdetection/base/BaseViewModel.kt
@@ -4,21 +4,43 @@ import android.app.Application
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.objectdetection.ext.uiScope
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.stateIn
+import kotlinx.coroutines.launch
 
 
-abstract class BaseViewModel(app: Application) : AndroidViewModel(app) {
+abstract class BaseViewModel : ViewModel() {
 
-    private val _viewStateLiveData = MutableLiveData<ViewState>()
-    val viewStateLiveData: LiveData<ViewState> = _viewStateLiveData
+    private val _viewState = MutableStateFlow<ViewState>(ViewState.Idle)
+    val viewState: StateFlow<ViewState> = _viewState.stateIn(
+        scope = viewModelScope,
+        started = SharingStarted.WhileSubscribed(5000),
+        initialValue = ViewState.Idle
+    )
 
-    protected fun viewStateChanged(viewState: ViewState) {
-        uiScope {
-            _viewStateLiveData.value = viewState
-            _viewStateLiveData.value = null
-        }
+    private val _viewEvent = MutableSharedFlow<ViewEvent>()
+    val viewEvent: Flow<ViewEvent> = _viewEvent
+
+    protected fun onChangedViewState(state: ViewState) = viewModelScope.launch {
+        _viewState.value = state
     }
 
+    protected fun onChangedViewEvent(event: ViewEvent) = viewModelScope.launch {
+        _viewEvent.emit(event)
+    }
 }
 
-interface ViewState
+interface ViewState {
+    object Idle : ViewState
+}
+
+interface ViewEvent {
+    data class ShowToast(val message: String) : ViewEvent
+}
