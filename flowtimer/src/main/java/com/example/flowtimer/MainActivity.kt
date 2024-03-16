52c154a2f5a0b834b542ad0c30fb9eb423a1458c
Kim Kyungsik <ksiki1@naver.com>
2024-03-16 19:36:16 +0900

    Flow Channel Practice

--- flowtimer/src/main/java/com/example/flowtimer/MainActivity.kt
+++ flowtimer/src/main/java/com/example/flowtimer/MainActivity.kt
@@ -2,10 +2,38 @@ package com.example.flowtimer
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.lifecycleScope
+import kotlinx.coroutines.channels.Channel
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+    val channel = Channel<Int>()
+
+
+    lifecycleScope.launch{
+        repeat(3){
+            delay(100)
+            channel.send(it)
+            println("send: $it")
+        }
     }
+
+        repeat(3){
+            delay(300)
+            println("receive ${channel.receive()}")
+        }
+
+
+    }
+
+
 }
+
+
+
