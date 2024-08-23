f11dfc424f440372e0d6d682ec48ea13791f7f3e
Kim Kyungsik <ksiki1@naver.com>
2024-08-23 15:21:49 +0900

    불필요한 부분 제거

--- app/src/main/java/com/example/objectdetection/ext/ViewExt.kt
+++ app/src/main/java/com/example/objectdetection/ext/ViewExt.kt
@@ -41,22 +41,6 @@ fun TextInputLayout.hideError() {
     isErrorEnabled = false
 }
 
-fun Context.checkPermission(callback: (isGranted: Boolean) -> Unit) {
-    TedPermission.create()
-        .setPermissionListener(object : PermissionListener {
-            override fun onPermissionGranted() {
-                callback.invoke(true)
-            }
-
-            override fun onPermissionDenied(deniedPermissions: MutableList<String>?) {
-                callback.invoke(false)
-            }
-        })
-        .setPermissions(*providePermissions())
-        .setDeniedMessage("If you reject permission")
-        .check()
-}
-
 fun providePermissions(): Array<String> {
     return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
         arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE)
