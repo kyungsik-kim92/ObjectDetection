9464759dc381108062fcfced910be821aa4cc9e7
Kim Kyungsik <ksiki1@naver.com>
2024-08-24 09:31:28 +0900

    모듈 간 의존성 추가

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -46,6 +46,7 @@ android {
 }
 
 dependencies {
+    implementation(project(":presentation"))
 
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
