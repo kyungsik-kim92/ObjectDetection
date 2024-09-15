ae293261a37745c7b7594aa24a221ee6bd6575cd
Kim Kyungsik <ksiki1@naver.com>
2024-09-15 19:02:26 +0900

    불필요한 파일삭제

--- app/src/main/java/com/example/objectdetection/data/repo/FirebaseRepository.kt
+++
@@ -1,6 +0,0 @@
-package com.example.objectdetection.data.repo
-
-import com.example.objectdetection.data.source.remote.FirebaseRemoteDataSource
-
-interface FirebaseRepository : FirebaseRemoteDataSource {
-}
