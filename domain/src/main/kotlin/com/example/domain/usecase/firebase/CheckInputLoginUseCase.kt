7ed5e5304dcf5300df89f4fb0573cb9f7c8900e6
Kim Kyungsik <ksiki1@naver.com>
2024-09-22 23:25:33 +0900

    CheckInputLoginUseCase

---
+++ domain/src/main/kotlin/com/example/domain/usecase/firebase/CheckInputLoginUseCase.kt
@@ -0,0 +1,31 @@
+package com.example.domain.usecase.firebase
+
+class CheckInputLoginUseCase {
+    operator fun invoke(email: String, password: String): CheckLoginState {
+        if (email.isEmpty()) {
+            return CheckLoginState.Failure(LoginErrorType.NotInputEmail)
+        }
+        if (password.isEmpty()) {
+            return CheckLoginState.Failure(LoginErrorType.NotInputPassword)
+        }
+        if (!android.util.Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+            return CheckLoginState.Failure(LoginErrorType.InvalidEmail)
+        }
+        return CheckLoginState.Success
+    }
+}
+
+sealed
+
+interface CheckLoginState {
+    data class Failure(val type: LoginErrorType) : CheckLoginState
+    data object Success : CheckLoginState
+
+}
+
+sealed interface LoginErrorType {
+    data object NotInputEmail : LoginErrorType
+    data object InvalidEmail : LoginErrorType
+    data object NotInputPassword : LoginErrorType
+
+}
