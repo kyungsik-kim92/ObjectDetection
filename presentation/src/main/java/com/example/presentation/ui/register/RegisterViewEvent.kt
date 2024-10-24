43b8b28ea50dea6cc7b61c31a94ce20bdb1f62c2
Kim Kyungsik <ksiki1@naver.com>
2024-10-24 15:02:40 +0900

    RegisterViewEvent

---
+++ presentation/src/main/java/com/example/presentation/ui/register/RegisterViewEvent.kt
@@ -0,0 +1,15 @@
+package com.example.presentation.ui.register
+
+import com.example.presentation.base.ViewEvent
+import com.example.presentation.base.ViewState
+
+data class RegisterViewState(
+    val isEnable: Boolean = true,
+    val isLoading: Boolean = false
+) : ViewState
+
+
+sealed interface RegisterViewEvent : ViewEvent {
+    data object RouteHome : RegisterViewEvent
+    data class Error(val message: String) : RegisterViewEvent
+}
