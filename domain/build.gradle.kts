499dbbd14bbd9b2274f06d581f68c7d10fdefdd9
Kim Kyungsik <ksiki1@naver.com>
2024-10-24 17:34:54 +0900

    Gson 추가

--- domain/build.gradle.kts
+++ domain/build.gradle.kts
@@ -53,4 +53,7 @@ dependencies {
 
     implementation("com.google.dagger:hilt-android:2.48.1")
     kapt("com.google.dagger:hilt-android-compiler:2.48.1")
+
+    implementation("com.google.code.gson:gson:2.8.9")
+
 }
