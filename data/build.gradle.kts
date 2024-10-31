b234e25a1687f9f561122788bfa18aef0a8795a3
Kim Kyungsik <ksiki1@naver.com>
2024-10-31 23:12:44 +0900

    room 라이브러리

--- data/build.gradle.kts
+++ data/build.gradle.kts
@@ -57,4 +57,9 @@ dependencies {
 
     implementation("com.squareup.retrofit2:retrofit:2.9.0")
     implementation("com.squareup.retrofit2:converter-gson:2.9.0")
+
+    implementation("androidx.room:room-runtime:2.6.1")
+    annotationProcessor("androidx.room:room-compiler:2.6.1")
+    kapt("androidx.room:room-compiler:2.6.1")
+    implementation("androidx.room:room-ktx:2.6.1")
 }
