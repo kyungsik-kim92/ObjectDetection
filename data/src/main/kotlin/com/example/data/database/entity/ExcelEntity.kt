467642840e106be138ec5c4d37ca8a1d87f7b456
Kim Kyungsik <ksiki1@naver.com>
2024-11-03 23:28:02 +0900

    ExcelEntity

--- data/src/main/kotlin/com/example/data/database/entity/ExcelEntity.kt
+++ data/src/main/kotlin/com/example/data/database/entity/ExcelEntity.kt
@@ -3,6 +3,8 @@ package com.example.data.database.entity
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 import com.example.data.database.DBTable
+import com.example.model.WordItem
+import com.example.model.api.ExcelResponse
 
 
 @Entity(tableName = DBTable.EXCEL_TABLE_NAME)
@@ -10,4 +12,10 @@ data class ExcelEntity(
     @PrimaryKey
     val word: String,
     val mean: String
-)
+) {
+    fun toWordItem(): WordItem =
+        WordItem(word, mean)
+}
+
+fun ExcelResponse.toExcelEntity(): ExcelEntity =
+    ExcelEntity(word, mean)
