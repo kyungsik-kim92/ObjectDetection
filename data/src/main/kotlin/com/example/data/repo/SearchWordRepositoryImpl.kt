e3feea361026067f5a34c1b8a20f0fbcfa008c17
Kim Kyungsik <ksiki1@naver.com>
2024-11-09 23:30:46 +0900

    SearchWordRepository

--- data/src/main/kotlin/com/example/data/repo/SearchWordRepositoryImpl.kt
+++ data/src/main/kotlin/com/example/data/repo/SearchWordRepositoryImpl.kt
@@ -1,20 +1,32 @@
 package com.example.data.repo
 
 
+import com.example.data.database.entity.toExcelEntity
+import com.example.data.source.local.SearchWordLocalDataSource
 import com.example.data.source.remote.SearchWordRemoteDataSource
 import com.example.domain.repo.SearchWordRepository
+import com.example.model.WordItem
 import com.example.model.api.DictionaryResponse
-import com.example.model.api.ExcelResponse
 import com.example.model.common.Result
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.map
 import javax.inject.Inject
 
 class SearchWordRepositoryImpl @Inject constructor(
-    private val searchWordRemoteDataSource: SearchWordRemoteDataSource
+    private val searchWordRemoteDataSource: SearchWordRemoteDataSource,
+    private val searchWordLocalDataSource: SearchWordLocalDataSource
 ) : SearchWordRepository {
 
-    override val excelList: Flow<List<ExcelResponse>>
-        get() = searchWordRemoteDataSource.excelList
+    override val excelList: Flow<List<WordItem>>
+        get() = searchWordLocalDataSource.excelList.map { list ->
+            list.map { item -> item.toWordItem() }
+        }
+
+    override suspend fun updateExcelList() {
+        searchWordLocalDataSource.inserts(
+            searchWordRemoteDataSource.getExcelData().map { it.toExcelEntity() })
+
+    }
 
     override suspend fun searchMeanWord(word: String): Result<DictionaryResponse> =
         searchWordRemoteDataSource.searchMeanWord(word)
