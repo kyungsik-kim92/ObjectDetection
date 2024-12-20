7c7db3d515f7d96b46013412d639360022d28826
Kim Kyungsik <ksiki1@naver.com>
2024-12-21 08:04:15 +0900

    CameraFragment 주석.

--- presentation/src/main/java/com/example/presentation/ui/search/detect/CameraFragment.kt
+++ presentation/src/main/java/com/example/presentation/ui/search/detect/CameraFragment.kt
@@ -54,13 +54,13 @@ class CameraFragment : Fragment(),
         super.onResume()
         if (!PermissionFragment.hasPermissions(requireContext())) {
             routePermission()
-        }
+        } // 권한이 없으면 권한 요청 화면 이동
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
         cameraExecutor.shutdown()
-    }
+    } //Fragment가 파괴 될때 Executor 종료
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
@@ -74,8 +74,8 @@ class CameraFragment : Fragment(),
     private fun initObject() {
         objectDetectorHelper =
             ObjectDetectorHelper(context = requireContext(), objectDetectorListener = this)
-
-        cameraExecutor = Executors.newSingleThreadExecutor()
+            // objectDetectorHelper 초기화
+        cameraExecutor = Executors.newSingleThreadExecutor() // 단일 스레드 Executor 초기화
     }
 
     private fun setUpCamera() {
@@ -87,29 +87,29 @@ class CameraFragment : Fragment(),
                 bindCameraUseCases()
 
             },
-            ContextCompat.getMainExecutor(requireContext())
+            ContextCompat.getMainExecutor(requireContext()) // 메인 스레드에서 실행
         )
-    }
+    } // cameraProvider 초기화
 
     private fun bindCameraUseCases() {
 
 
         val cameraProvider = cameraProvider
-            ?: throw IllegalStateException("Camera initialization failed.")
+            ?: throw IllegalStateException("Camera initialization failed.") // cameraProvider가 없으면 예외 발생
 
         val cameraSelector =
-            CameraSelector.Builder().requireLensFacing(CameraSelector.LENS_FACING_BACK).build()
+            CameraSelector.Builder().requireLensFacing(CameraSelector.LENS_FACING_BACK).build() // 후방 카메라 선택
 
         preview = Preview.Builder()
             .setTargetAspectRatio(AspectRatio.RATIO_4_3)
             .setTargetRotation(binding.viewFinder.display.rotation)
-            .build()
+            .build() // 프리뷰 비율 4:3으로 설정, 화면 회전에 맞게 설정
 
         imageAnalyzer = ImageAnalysis.Builder()
-            .setTargetAspectRatio(AspectRatio.RATIO_4_3)
-            .setTargetRotation(binding.viewFinder.display.rotation)
-            .setBackpressureStrategy(ImageAnalysis.STRATEGY_KEEP_ONLY_LATEST)
-            .setOutputImageFormat(ImageAnalysis.OUTPUT_IMAGE_FORMAT_RGBA_8888)
+            .setTargetAspectRatio(AspectRatio.RATIO_4_3) //이미지 분석 비율 4:3 설정
+            .setTargetRotation(binding.viewFinder.display.rotation) //화면 회전에 맞게 설정
+            .setBackpressureStrategy(ImageAnalysis.STRATEGY_KEEP_ONLY_LATEST) //최신 이미지로 이전 이미지 덮어쓰기
+            .setOutputImageFormat(ImageAnalysis.OUTPUT_IMAGE_FORMAT_RGBA_8888) // 출력 이미지 형식
             .build()
             .also {
                 it.setAnalyzer(cameraExecutor) { image ->
@@ -119,17 +119,17 @@ class CameraFragment : Fragment(),
                             image.height,
                             Bitmap.Config.ARGB_8888
                         )
-                    }
+                    } // 비트맵 버퍼 초기화
 
-                    detectObjects(image)
+                    detectObjects(image) // 객체 감지
                 }
             }
 
-        cameraProvider.unbindAll()
+        cameraProvider.unbindAll() // 카메라 사용 해제
 
         try {
-            camera = cameraProvider.bindToLifecycle(this, cameraSelector, preview, imageAnalyzer)
-            preview?.setSurfaceProvider(binding.viewFinder.surfaceProvider)
+            camera = cameraProvider.bindToLifecycle(this, cameraSelector, preview, imageAnalyzer) // 카메라 라이프사이클에 바인딩
+            preview?.setSurfaceProvider(binding.viewFinder.surfaceProvider) // 프리뷰 표시
         } catch (e: Exception) {
             e.printStackTrace()
         }
@@ -139,16 +139,16 @@ class CameraFragment : Fragment(),
         // Copy out RGB bits to the shared bitmap buffer
         image.use { bitmapBuffer.copyPixelsFromBuffer(image.planes[0].buffer) }
 
-        val imageRotation = image.imageInfo.rotationDegrees
+        val imageRotation = image.imageInfo.rotationDegrees // 이미지 회전
         // Pass Bitmap and rotation to the object detector helper for processing and detection
-        objectDetectorHelper.detect(bitmapBuffer, imageRotation)
+        objectDetectorHelper.detect(bitmapBuffer, imageRotation) // 객체 감지
     }
 
 
     override fun onConfigurationChanged(newConfig: Configuration) {
         super.onConfigurationChanged(newConfig)
         imageAnalyzer?.targetRotation = binding.viewFinder.display.rotation
-    }
+    } // 이미지 분석기 회전 업데이트
 
     override fun onError(error: String) {
         Toast.makeText(requireContext(), error, Toast.LENGTH_SHORT).show()
@@ -166,9 +166,9 @@ class CameraFragment : Fragment(),
                 results ?: LinkedList<Detection>(),
                 imageHeight,
                 imageWidth
-            )
-            getTouchItem { routeSelectItem(it) }
+            ) // 결과가 null 이면 빈 리스트
+            getTouchItem { routeSelectItem(it) } // 터치된 아이템 처리
             invalidate()
         }
-    }
+    } //
 }
