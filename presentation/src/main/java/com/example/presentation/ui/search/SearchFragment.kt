ffc309bc5f4e9448f15045592bf945f70dc33d84
KKS <ksiki1@naver.com>
2025-08-25 23:54:21 +0900

    SearchFragment 수정

--- presentation/src/main/java/com/example/presentation/ui/search/SearchFragment.kt
+++ presentation/src/main/java/com/example/presentation/ui/search/SearchFragment.kt
@@ -5,7 +5,6 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import com.example.presentation.databinding.FragmentSearchBinding
 import com.example.presentation.ui.search.detect.DetectActivity
@@ -14,29 +13,36 @@ import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class SearchFragment : Fragment() {
+    private var _binding: FragmentSearchBinding? = null
+    private val binding get() = _binding!!
 
-    lateinit var binding: FragmentSearchBinding
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        binding = FragmentSearchBinding.inflate(inflater)
+        _binding = FragmentSearchBinding.inflate(inflater, container, false)
         return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        initUi()
+    }
+
+    private fun initUi() {
         binding.ivDetect.setOnClickListener {
-            startActivity<DetectActivity>()
-
+            startActivity(Intent(requireActivity(), DetectActivity::class.java))
         }
+
         binding.ivWord.setOnClickListener {
-            startActivity<WordActivity>()
+            startActivity(Intent(requireActivity(), WordActivity::class.java))
         }
     }
 
-    private inline fun <reified T : AppCompatActivity> startActivity() {
-        startActivity(Intent(requireActivity(), T::class.java))
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
+
 }
