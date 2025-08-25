1b62276e9793bcff21870b84694f516e4e6e203f
KKS <ksiki1@naver.com>
2025-08-25 23:50:21 +0900

    SplashFragment 수정

--- presentation/src/main/java/com/example/presentation/ui/splash/SplashFragment.kt
+++ presentation/src/main/java/com/example/presentation/ui/splash/SplashFragment.kt
@@ -1,39 +1,70 @@
 package com.example.presentation.ui.splash
 
 import android.os.Bundle
+import android.view.LayoutInflater
 import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
-import com.example.presentation.R
-import com.example.presentation.base.BaseFragment
-import com.example.presentation.base.ViewEvent
-import com.example.presentation.base.ViewState
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
 import com.example.presentation.databinding.FragmentSplashBinding
 import com.example.presentation.ext.routeLoginFragment
 import com.example.presentation.ui.mypage.MyPageFragment.Companion.KEY_LOGOUT
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.launch
 
 
 @AndroidEntryPoint
-class SplashFragment : BaseFragment<FragmentSplashBinding>(R.layout.fragment_splash) {
+class SplashFragment : Fragment() {
 
-    override val viewModel by viewModels<SplashViewModel>()
+    private var _binding: FragmentSplashBinding? = null
+    private val binding get() = _binding!!
+
+    private val splashViewModel by viewModels<SplashViewModel>()
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentSplashBinding.inflate(inflater, container, false)
+        return binding.root
+    }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        checkLogoutIntent()
+        observeUiState()
+    }
+
+    private fun checkLogoutIntent() {
         if (requireActivity().intent.getBooleanExtra(KEY_LOGOUT, false)) {
             routeLoginFragment()
         }
     }
 
-    override fun initUi() {}
+    private fun observeUiState() {
+        lifecycleScope.launch {
+            viewLifecycleOwner.repeatOnLifecycle(Lifecycle.State.STARTED) {
+                splashViewModel.uiState.collect { state ->
+                    when (state) {
+                        is SplashUiState.Loading -> {}
 
-    override fun onChangedViewState(state: ViewState) {
-        when (state) {
-            is SplashViewState.RouteLogin -> routeLoginFragment()
+                        is SplashUiState.RouteLogin -> {
+                            routeLoginFragment()
+                        }
+                    }
+                }
+            }
         }
+
+
     }
 
-    override fun onChangeViewEvent(event: ViewEvent) {}
-
-
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
 }
