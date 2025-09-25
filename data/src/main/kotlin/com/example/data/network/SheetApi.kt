24f2a86703e5c0a3a6c974e5fc2f87bfaf8cea36
KKS <ksiki1@naver.com>
2025-09-25 23:32:58 +0900

    SheetApi Url 수정

--- data/src/main/kotlin/com/example/data/network/SheetApi.kt
+++ data/src/main/kotlin/com/example/data/network/SheetApi.kt
@@ -1,12 +1,11 @@
 package com.example.data.network
 
-import com.example.model.api.ExcelResponse
 import retrofit2.http.GET
 
 interface SheetApi {
 
     companion object {
-        private const val URL = "api/v1/ahfewsnq0tnw0"
+        private const val URL = "api/v1/1mbyz5l2rbbdk"
 
     }
 
