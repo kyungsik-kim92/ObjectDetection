392d0b51882c1a5c78f104b906f2316be909f04b
KKS <ksiki1@naver.com>
2025-09-26 18:27:14 +0900

    불필요한 부분 삭제

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,15 +1,4 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
-
-
-buildscript{
-    dependencies{
-        classpath("androidx.navigation:navigation-safe-args-gradle-plugin:2.7.7")
-        classpath("com.google.devtools.ksp:com.google.devtools.ksp.gradle.plugin:1.9.10-1.0.13")
-    }
-}
-
-
-
 plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.kotlin.android) apply false
