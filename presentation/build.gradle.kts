98ebf4b97f6719890a8f6d8c4cf55f8abc089a68
KKS <ksiki1@naver.com>
2025-12-03 22:23:20 +0900

    mlkitTextRecognition 라이브러리 추가

--- presentation/build.gradle.kts
+++ presentation/build.gradle.kts
@@ -92,6 +92,8 @@ dependencies {
     implementation(libs.androidx.camera.camera2)
     implementation(libs.androidx.camera.lifecycle)
     implementation(libs.androidx.camera.view)
+
+    implementation(libs.google.mlkit.text.recognition)
 }
 kapt {
     correctErrorTypes = true
