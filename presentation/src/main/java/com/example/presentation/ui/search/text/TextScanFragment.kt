0e23cdce629f4420e5232bb2d4fff77bb16942c0
KKS <ksiki1@naver.com>
2025-12-19 23:08:54 +0900

    TextScanFragment 구조 생성

--- presentation/src/main/java/com/example/presentation/ui/search/text/TextScanFragment.kt
+++ presentation/src/main/java/com/example/presentation/ui/search/text/TextScanFragment.kt
@@ -1,6 +1,41 @@
 package com.example.presentation.ui.search.text
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import com.example.presentation.databinding.FragmentTextScanBinding
 
 class TextScanFragment : Fragment() {
+    private var _binding: FragmentTextScanBinding? = null
+    private val binding get() = _binding!!
+
+    private val viewModel: TextScanViewModel by viewModels()
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentTextScanBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        initUi()
+    }
+
+    private fun initUi() {
+        binding.btnBack.setOnClickListener {
+            requireActivity().finish()
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
 }
